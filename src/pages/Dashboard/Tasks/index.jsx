83553b2acb17f3083079fec4f93c72c55d81cd02
VictorMarques98 <victormonreal98@gmail.com>
2021-05-21 11:19:28 -0300

    [*]create folders and default files

---
+++ src/pages/Dashboard/Tasks/index.jsx
@@ -0,0 +1,3 @@
+/* Eu criei esse componente da tela dashboard fora de components porque se esse componente for
+utilizado apenas na tela dashboard, não há necessidade de criar na pasta componentes
+A pasta components só vai os componentes que vamos utilizar em mais de uma página */
