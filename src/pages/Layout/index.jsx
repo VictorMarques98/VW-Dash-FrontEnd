0c89ec1693283c1f72d293c98fd00b289a98f562
VictorMarques98 <victormonreal98@gmail.com>
2021-05-24 18:59:27 -0300

    [*]create header & leftMenu componentes (basic layout)

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-
 import {
   HashRouter, Switch, Route, Redirect,
 } from 'react-router-dom';
 
-// pages
-
+// Pages
 import Dashboard from '../Dashboard';
+
+// Components
+import Header from '../../components/Header';
+import LeftMenu from '../../components/LeftMenu';
 
 function Layout () {
 
   return(
   <HashRouter>
-    <Switch>
-      <Route path="/dashboard" exact render={() => <Dashboard />} />
-      <Route exact path="/">
-        <Redirect to="/dashboard" />
-      </Route>
-    </Switch>
+    <LeftMenu />
+    <div className="appContent">
+      <Header />
+      <Switch>
+        <Route path="/dashboard" exact render={() => <Dashboard />} />
+        <Route exact path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </div>
   </HashRouter>
   )
 };
