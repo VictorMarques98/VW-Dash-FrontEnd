4d0d3b00751a7663e89878631856b3e742b52d2d
wellingtonollv <28570773+wellingtonollv@users.noreply.github.com>
2021-07-09 14:31:42 -0300

    dale123213

--- src/components/LeftMenu/LinkMenu.jsx
+++ src/components/LeftMenu/LinkMenu.jsx
@@ -6,7 +6,6 @@ export default function LinkMenu({ children, path }) {
   return (
     <div className="LinkMenuWrapper">
       <ul className="linkMenuList">
-
         <NavLink to={path} activeClassName="active">{children}</NavLink>
       </ul>
     </div>
